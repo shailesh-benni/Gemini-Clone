747571b71899cc27db2c1910ca40863705314a38
testing-gems <shailesh.butter@gmail.com>
2024-10-19 14:07:02 +0530

    Front-End

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from './components/SideBar/SideBar'
+import Main from './components/Main/Main'
+const App = () => {
+  return (
+    <>
+      <Sidebar/>
+      <Main/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./Main.css";
+import { assets } from "../../assets/assets";
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        <div className="greet">
+          <p>
+            <span>Hello, Shailu</span>
+          </p>
+          <p>How Can I Help You Today</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest Some Sci-fi Movies</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+
+          <div className="card">
+            <p>Brainstrom activites to boost our team work</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Brainstrom activites to boost our team work</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+
+          <div className="card">
+            <p>Improve the readability of the given code</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
+        </div>
+
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type="text" placeholder="Enter a Prompt Here" />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              <img src={assets.send_icon} alt="" />
+            </div>
+          </div>
+          <p className="bottom-info">
+            Gemini may display inaccurate info, including about people, so
+            double-check its responses. Your privacy and Gemini Apps
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
